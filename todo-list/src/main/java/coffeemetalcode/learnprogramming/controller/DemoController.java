394e3b53f1d0fc1587b8117fc01d7f3601fbbc85
dbstocker <coffeemetalcode@gmail.com>
2023-12-31 22:36:22 -0500

    lesson 59 challenge

--- todo-list/src/main/java/coffeemetalcode/learnprogramming/controller/DemoController.java
+++ todo-list/src/main/java/coffeemetalcode/learnprogramming/controller/DemoController.java
@@ -1,15 +1,50 @@
 package coffeemetalcode.learnprogramming.controller;
 
+import coffeemetalcode.learnprogramming.service.DemoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
 
+    /* fields */
+    private final DemoService demoService;
+
+    /* constructor */
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
+    /* request handlers */
     @ResponseBody
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    /* Passing Model in method and adding an attribute */
+    @GetMapping("welcome")
+    public String welcome(Model model) {
+        model.addAttribute("helloMessage", demoService.getHelloMessage("David"));
+        log.info("model = {}", model);
+
+        return "welcome";
+    }
+
+    /* Using the ModelAttribute decorator to confer an attribute */
+    /* model attribute */
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage() called");
+
+        return demoService.getWelcomeMessage();
+
     }
 }
---
+++ todo-list/src/main/webapp/WEB-INF/view/welcome.jsp
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>${helloMessage}</h1>
+    <h2>${welcomeMessage}</h2>
+    <p>Welcome page in JSP</p>
+</body>
+</html>
